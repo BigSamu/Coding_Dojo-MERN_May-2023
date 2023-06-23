b8568b1a373309686cbfc62d16f4f079c73b2303
Samuel Valdes Gutierrez <valdesgutierrez@gmail.com>
2023-06-23 14:17:36 -0400

    adding async problem

--- Clases/03_Functional_Programming/06_Async_Problem.js
+++ Clases/03_Functional_Programming/06_Async_Problem.js
@@ -1,4 +1,38 @@
-// I) Problem -> How I going to start Painting if I receive thre brush before the
-// paint?
+//------------------------------------------------------------------------------
+// ASYNC PROBLEM
+//------------------------------------------------------------------------------
 
 
+// Problem -> How I going to start Painting if I receive the brush before the
+// paint? Same if I receive the paint before the tarp? I need to wait!!!
+
+const orderSupplies = (item, callback) => {
+  let warehouse; //undefined
+  const deliveryTime = Math.random() * 3000;
+  setTimeout(() => {
+    warehouse = {
+      paint: {
+        product: "Neon Green Paint",
+        directions: () => "mix it!",
+      },
+      brush: {
+        product: "Horsehair brush",
+        directions: () => "start painting!",
+      },
+      tarp: {
+        product: "A large tarp",
+        directions: () => "cover the floor!",
+      },
+    };
+    callback(warehouse[item]);
+  }, deliveryTime);
+};
+const receivedItem = (item) => {
+  console.log(`Received ${item.product}, time to ${item.directions()}`);
+};
+
+orderSupplies("tarp", receivedItem);
+orderSupplies("paint", receivedItem);
+orderSupplies("brush", receivedItem);
+
+console.log("----------------------");
