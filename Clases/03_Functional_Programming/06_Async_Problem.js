5784fbb2a411409f03cdbf87732fe755652d92da
Samuel Valdes Gutierrez <valdesgutierrez@gmail.com>
2023-06-28 23:36:14 -0400

    class 28/06/23

--- Clases/03_Functional_Programming/06_Async_Problem.js
+++ Clases/03_Functional_Programming/06_Async_Problem.js
@@ -2,13 +2,17 @@
 // ASYNC PROBLEM
 //------------------------------------------------------------------------------
 
-
 // Problem -> How I going to start Painting if I receive the brush before the
 // paint? Same if I receive the paint before the tarp? I need to wait!!!
 
 const orderSupplies = (item, callback) => {
+  // -----------------------
+  // API simulation
+  //  ----------------------
   let warehouse; //undefined
-  const deliveryTime = Math.random() * 3000;
+  const deliveryTime = Math.random() * 3000; // random time between 0 and 3000
+  // simulate the time to get the item
+  // Simulate response from API
   setTimeout(() => {
     warehouse = {
       paint: {
@@ -24,15 +28,18 @@ const orderSupplies = (item, callback) => {
         directions: () => "cover the floor!",
       },
     };
+
     callback(warehouse[item]);
   }, deliveryTime);
 };
+
 const receivedItem = (item) => {
+  // item is a object
   console.log(`Received ${item.product}, time to ${item.directions()}`);
 };
 
-orderSupplies("tarp", receivedItem);
 orderSupplies("paint", receivedItem);
 orderSupplies("brush", receivedItem);
+orderSupplies("tarp", receivedItem);
 
-console.log("----------------------");
+console.log("---------------------------");
--- Clases/03_Functional_Programming/07_Promises_and_Async_Communication.js
+++ Clases/03_Functional_Programming/07_Promises_and_Async_Communication.js
@@ -1,10 +1,10 @@
 //------------------------------------------------------------------------------
-// PROMISES AND ASYN COMMUNICATION
+// PROMISES AND ASYNC COMMUNICATION
 //------------------------------------------------------------------------------
 
 // In JavaScript, a Promise is an object that represents the eventual completion
 // or failure of an asynchronous operation and its resulting value. Promises are
-// used to handle asynchronous operations in a more structured and readable way.
+// used to handle asynchronous communication in a more structured and readable way.
 
 // A Promise has 3 states:
 // a) Pending: Initial state, neither fulfilled nor rejected.
@@ -19,3 +19,50 @@ const promise = new Promise((resolve, reject) => {
   //  If the operation fails, call reject(error)
 });
 
+// Check what a promise contains
+console.log(promise);
+
+console.log("----------------------");
+
+// Example 1
+const fetchData = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    const data = "Hello World!";
+    resolve(data);
+  }, 1500);
+});
+
+// fetchData
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
+
+// Example 2
+const fetchData2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    const data = {
+      id: 1,
+      name: "John Doe",
+      age: 30,
+    }; /* Simulated API response */
+    const status = "ok"; /* Simulated API OK status */
+    // const status = "error"; /* Simulated API ERROR status */
+
+    if (status === "ok") {
+      resolve(data);
+    } else {
+      reject("Error: Something went wrong");
+    }
+  }, 2000);
+});
+
+fetchData2
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((error) => {
+    console.log("Error:", error);
+  });
